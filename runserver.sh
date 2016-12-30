ccdc1ed0ff90f3ce74bf09358950ccb9bd06c817
Jacob Errington <git@mail.jerrington.me>
2016-12-30 17:15:47 -0500

    scrap .secrets file in favour of parsing MPD_HOST

--- runserver.sh
+++ runserver.sh
@@ -2,12 +2,30 @@
 
 set -e
 
-source .secrets
+PASSWORD="$(echo "$MPD_HOST" | cut -d "@" -f 1)"
+
+if [ "$PASSWORD" = "$MPD_HOST" ] ; then
+  PASSWORD=
+  HOSTPORT="$MPD_HOST"
+else
+  HOSTPORT="$(echo "$MPD_HOST" | cut -d "@" -f 2)"
+fi
+
+HOST="$(echo "$HOSTPORT" | cut -d ":" -f 1)"
+
+if [ "$HOST" = "$HOSTPORT" ] ; then
+  PORT=6600
+else
+  PORT="$(echo "$HOSTPORT" | cut -d ":" -f 2)"
+fi
 
 DBDIR="$1"
 if test -z "$DBDIR" ; then
+  echo 'No database directory set. Defaulting to ./db.'
   DBDIR=db
 fi
+
+echo $HOST $PORT $PASSWORD
 
 D="$PWD"
 mkdir -p $DBDIR/{music,transcoded,archives}
